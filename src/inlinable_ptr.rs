05275d4546b44d84452f814356fa80140add6d8b
threadexception <hannes.gaumann@outlook.de>
2022-12-15 22:38:02 +0100

    Make it work

--- src/inlinable_ptr.rs
+++ src/inlinable_ptr.rs
@@ -2,25 +2,25 @@ use std::cell::UnsafeCell;
 use std::{mem, ptr};
 use std::boxed::ThinBox;
 use std::mem::{align_of, ManuallyDrop, MaybeUninit, size_of, transmute};
+use std::ops::{Deref, DerefMut};
 use std::pin::Pin;
 use std::ptr::NonNull;
 
-pub type SlimPtrMutDrop<T, const DROP: bool> = SlimPtrMutGeneric<T, |val| {
-    ThinBox::new()
-}, DROP>;
+// TODO: can we remove the `unchecked`s or are they required for the compiler to elide the bound-checks?
 
-pub trait Creator {}
-
-impl<I: FnMut(T) -> NonNull<T>, T> Creator for I {}
-
-pub trait CleanUp {}
-
-impl<I: FnMut(NonNull<T>) -> T, T> CleanUp for I {}
+fn create_thin_box<T>() -> impl FnMut(T) -> NonNull<T> {
+    |val| {
+        let bx = ThinBox::new(val);
+        let ret = NonNull::new(ret.deref() as *mut T).unwrap();
+        mem::forget(bx);
+        ret
+    }
+}
 
 // repr(C) won't impact perf negatively here as it only disallows field reordering optimizations
 // which aren't a thing anyways in this struct with only one `real` (non-zero sized) field
 #[repr(C)]
-pub struct SlimPtrMutGeneric<T, const Create: impl FnMut(T) -> NonNull<T>, const CleanUp: impl FnMut(NonNull<T>) -> T, const DROP: bool = false> {
+pub struct SlimPtrMut<T> {
     ptr: [NonNull<T>; const {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
             1
@@ -37,13 +37,13 @@ pub struct SlimPtrMutGeneric<T, const Create: impl FnMut(T) -> NonNull<T>, const
     }],
 }
 
-impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, const CleanUp: CU, const DROP: bool> SlimPtrMutGeneric<T, C, CU, Create, CleanUp> {
+impl<T> SlimPtrMut<T> {
 
     #[inline]
-    pub fn new(val: T) -> Self {
+    pub fn new_with(val: T, create: impl FnMut(T) -> NonNull<T>) -> Self {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
             Self {
-                ptr: [Create(val)],
+                ptr: [create(val)],
                 val: [],
             }
         } else {
@@ -57,10 +57,8 @@ impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, 
     #[inline]
     pub fn as_ptr(&self) -> *const T {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.ptr.get_unchecked(0).as_ptr() }
         } else {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.val.get_unchecked(0) as *const T }
         }
     }
@@ -68,17 +66,10 @@ impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, 
     #[inline]
     pub fn as_ptr_mut(&mut self) -> *mut T {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.ptr.get_unchecked(0).as_ptr() }
         } else {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.val.get_unchecked(0) as *mut T }
         }
-    }
-
-    #[inline]
-    pub fn as_ref(&self) -> &T {
-        unsafe { &*self.as_ptr() }
     }
 
     #[inline]
@@ -94,36 +85,56 @@ impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, 
         }
     }
 
-    pub fn into_val(mut self) -> T {
+}
+
+impl<T> AsRef<T> for SlimPtrMut<T> {
+    #[inline]
+    fn as_ref(&self) -> &T {
+        unsafe { &*self.as_ptr() }
+    }
+}
+
+impl<T> AsMut<T> for SlimPtrMut<T> {
+    #[inline]
+    fn as_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.as_ptr_mut() }
+    }
+}
+
+impl<T> Into<T> for SlimPtrMut<T> {
+    #[inline]
+    fn into(self) -> T {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
             unsafe { self.ptr.get_unchecked(0).as_ptr().read() }
         } else {
             // let ret = unsafe { self.val[0].0 };
-            let ret = unsafe { (self.val.get_unchecked(0) as *mut T).read() };
+            let ret = unsafe { (self.val.get_unchecked(0) as *mut ManuallyDrop<T>).read() }.into();
             mem::forget(self);
             ret
         }
     }
-
 }
 
-impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, const CleanUp: CU, const DROP: bool> Drop for SlimPtrMutGeneric<T, C, CU, Create, CleanUp> {
-    fn drop(&mut self) {
-        if DROP {
-            if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-                unsafe { CleanUp(*self.ptr.get_unchecked(0)) };
-            } else {
-                // FIXME: can needs_drop improve the perf here or is the compiler able to figure this out on its own?
-                unsafe { (self.val.get_unchecked(0) as *mut T).read() };
-            }
-        }
+impl<T> Deref for SlimPtrMut<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+impl<T> DerefMut for SlimPtrMut<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
     }
 }
 
 // repr(C) won't impact perf negatively here as it only disallows field reordering optimizations
 // which aren't a thing anyways in this struct with only one `real` (non-zero sized) field
 #[repr(C)]
-pub struct SlimPtrGeneric<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, const CleanUp: CU, const DROP: bool = false> {
+pub struct SlimPtr<T> {
     ptr: [NonNull<T>; const {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
             1
@@ -140,13 +151,13 @@ pub struct SlimPtrGeneric<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) ->
     }],
 }
 
-impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, const CleanUp: CU, const DROP: bool> SlimPtrGeneric<T, C, CU, Create, CleanUp> {
+impl<T> SlimPtr<T> {
 
     #[inline]
-    pub fn new(val: T) -> Self {
+    pub fn new_with(val: T, create: impl FnMut(T) -> NonNull<T>) -> Self {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
             Self {
-                ptr: [Create(val)],
+                ptr: [create(val)],
                 val: [],
             }
         } else {
@@ -160,43 +171,47 @@ impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, 
     #[inline]
     pub fn as_ptr(&self) -> *const T {
         if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.ptr.get_unchecked(0).as_ptr() }
         } else {
-            // FIXME: can we remove the `unchecked` here?
             unsafe { self.val.get_unchecked(0) as *const T }
-        }
-    }
-
-    #[inline]
-    pub fn as_ref(&self) -> &T {
-        unsafe { &*self.as_ptr() }
-    }
-
-    pub fn into_val(mut self) -> T {
-        if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-            unsafe { self.ptr.get_unchecked(0).as_ptr().read() }
-        } else {
-            // let ret = unsafe { self.val[0].0 };
-            let ret = unsafe { (self.val.get_unchecked(0) as *mut T).read() };
-            mem::forget(self);
-            ret
         }
     }
 
 }
 
-impl<T, C: FnMut(T) -> NonNull<T>, CU: FnMut(NonNull<T>) -> T, const Create: C, const CleanUp: CU, const DROP: bool> Drop for SlimPtrGeneric<T, C, CU, Create, CleanUp> {
-    fn drop(&mut self) {
-        if DROP {
-            if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
-                unsafe { CleanUp(*self.ptr.get_unchecked(0)) };
-            } else {
-                // FIXME: can needs_drop improve the perf here or is the compiler able to figure this out on its own?
-                unsafe { (self.val.get_unchecked(0) as *const T).read() };
-            }
+impl<T> AsRef<T> for SlimPtr<T> {
+    #[inline]
+    fn as_ref(&self) -> &T {
+        unsafe { &*self.as_ptr() }
+    }
+}
+
+impl<T> Into<T> for SlimPtr<T> {
+    #[inline]
+    fn into(self) -> T {
+        if size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>() {
+            unsafe { self.ptr.get_unchecked(0).as_ptr().read() }
+        } else {
+            // let ret = unsafe { self.val[0].0 };
+            let ret = unsafe { (self.val.get_unchecked(0) as *mut ManuallyDrop<T>).read() }.into();
+            mem::forget(self);
+            ret
         }
     }
+}
+
+impl<T> Deref for SlimPtr<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+#[inline]
+pub fn needs_cleanup<T>() -> bool {
+    size_of::<T>() > size_of::<NonNull<T>>() || align_of::<T>() > align_of::<NonNull<T>>()
 }
 
 #[repr(packed)]
